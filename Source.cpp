af5f98bdc5dc5521f626cb04ca5cdf2bfd6ee4e8
Elena Stepanova <lady.alena-he2015@yandex.ru>
2022-08-17 23:27:53 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+
+#ifdef _WIN32
+	system("chcp 1251");
+#endif
+
+	std::string name;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << std::endl;
+
+	return 0;
+}
